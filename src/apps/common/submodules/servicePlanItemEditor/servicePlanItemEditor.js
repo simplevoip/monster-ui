36ba4da1c991020663292d162d70951789aba72a
Joris Tirado <joris@tirado.com>
2020-03-08 19:01:25 -0700

    Create and register submodule

---
+++ src/apps/common/submodules/servicePlanItemEditor/servicePlanItemEditor.js
@@ -0,0 +1,18 @@
+define(function(require) {
+	return {
+		subscribe: {
+			'common.servicePlanItemEditor.render': 'servicePlanItemEditorRender'
+		},
+
+		/**
+		 * @param  {Object} args
+		 * @param  {String} args.caterogy
+		 * @param  {String} args.item
+		 * @param  {Object} [args.data={}]
+		 * @param  {Object} [args.editable]
+		 * @param  {Function} args.callback
+		 */
+		servicePlanItemEditorRender: function(args) {
+		}
+	};
+});
