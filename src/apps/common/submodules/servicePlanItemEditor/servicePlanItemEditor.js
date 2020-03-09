b786f1e031803e87257a0a93d8292ef2a70a20c5
Joris Tirado <joris@tirado.com>
2020-03-08 19:05:50 -0700

    Declare store getter/setter

--- src/apps/common/submodules/servicePlanItemEditor/servicePlanItemEditor.js
+++ src/apps/common/submodules/servicePlanItemEditor/servicePlanItemEditor.js
@@ -5,6 +5,42 @@ define(function(require) {
 		},
 
 		/**
+		 * Store getter
+		 * @param  {Array|String} [path]
+		 * @param  {*} [defaultValue]
+		 * @return {*}
+		 */
+		servicePlanItemEditorGetStore: function(path, defaultValue) {
+			var self = this,
+				store = ['_store', 'servicePlanItemEditor'];
+			return _.get(
+				self,
+				_.isUndefined(path)
+					? store
+					: _.flatten([store, _.isString(path) ? path.split('.') : path]),
+				defaultValue
+			);
+		},
+
+		/**
+		 * Store setter
+		 * @param  {Array|String} [path]
+		 * @param  {*} [value]
+		 */
+		servicePlanItemEditorSetStore: function(path, value) {
+			var self = this,
+				hasValue = _.toArray(arguments).length === 2,
+				store = ['_store', 'servicePlanItemEditor'];
+			_.set(
+				self,
+				hasValue
+					? _.flatten([store, _.isString(path) ? path.split('.') : path])
+					: store,
+				hasValue ? value : path
+			);
+		},
+
+		/**
 		 * @param  {Object} args
 		 * @param  {String} args.caterogy
 		 * @param  {String} args.item
