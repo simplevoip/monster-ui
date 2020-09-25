5140b9ac2a749ae26041f6e5b330053cd2350479
Andy Wells <andy.wells@gmail.com>
2020-09-24 23:44:47 -0500

    Update sv api endpoint

--- src/js/config.js
+++ src/js/config.js
@@ -35,6 +35,6 @@ define({
     },
     api: {
             'default' : 'https://portal.simplevoip.us:8443/v2/',
-            simplevoip: 'http://svportal.local/monster/'
+            simplevoip: 'https://staging.simplevoip.us/monster/'
     }
 });
